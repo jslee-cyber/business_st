e1cac306baf7874b2d7aa76eea36149d9a60f8d9
jslee-cyber <cullinan700@gmail.com>
2025-05-13 17:44:45 +0900

    Update streamlit_basic.py

--- streamlit_basic.py
+++ streamlit_basic.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title('첫번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
-st.title('fghjfghjf첫번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
-st.title('fghjfghjf첫번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
+st.title('두번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
+st.title('세번째 웹 어플 만들기 👋📺🙋🏻‍♂️')
